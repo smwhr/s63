80d2d6ab9a0f5fe3117169add892e8b0850bdec1
Pi/Smwhr <pi@troisyaourts.com>
2021-05-26 11:22:01 +0000

    update with inkplayer, sounds and improvements

--- buttons.py
+++ buttons.py
@@ -11,7 +11,7 @@ def say_hello():
 def say_goodbye():
     print("Goodbye!")
 
-comb = Button(3)
+comb = Button(23)
 
 comb.when_pressed = say_hello
 comb.when_released = say_goodbye
@@ -19,22 +19,22 @@ comb.when_released = say_goodbye
 c1 = Button(11)
 c1.when_pressed = lambda b: pressed("c1")
 
-c2 = Button(25)
+c2 = Button(9)
 c2.when_pressed = lambda b: pressed("c2")
 
-c3 = Button(9)
+c3 = Button(10)
 c3.when_pressed = lambda b: pressed("c3")
 
-l1 = Button(19)
+l1 = Button(7)
 l1.when_pressed = lambda b: pressed("l1")
 
-l2 = Button(16)
+l2 = Button(8)
 l2.when_pressed = lambda b: pressed("l2")
 
-l3 = Button(6)
+l3 = Button(25)
 l3.when_pressed = lambda b: pressed("l3")
 
-l4 = Button(20)
+l4 = Button(24)
 l4.when_pressed = lambda b: pressed("l4")
 
 print("Phone is ready")
